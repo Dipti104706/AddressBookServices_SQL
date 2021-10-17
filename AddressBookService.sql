59206642e5030311d95f6f919ebcb51c7a39c26e
Dipti104706 <diptimayeebehura104@gmail.com>
2021-10-18 00:51:20 +0530

    [Dipti] added code to update existing contacts in addressbook table

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -25,3 +25,12 @@ Insert into AddressBookTable values('Sarika','Boxi','Jhirpani','Rourkela','Odish
 ('Debasmita','Sutar','Jagda','Rourkela','Odisha',769042,258721564,'deba12@gmail.com'),
 ('Suchitra','Mallick','Kalpana','Bhubaneswar','Odisha',751042,458721564,'suchim12@gmail.com');
 select * from AddressBookTable;
+
+
+--------------------UC4----------------------------------------
+------edit existing contact person using person name----------
+update AddressBookTable set Phonenum=7845712874 where FirstName='Sujata';
+update AddressBookTable set LastName='Malick' where FirstName='Suchitra';
+update AddressBookTable set EmailId='smita123@gmail.com' where LastName='Sutar';
+update AddressBookTable set ZipCode='751185' where FirstName='Suchitra';
+select * from AddressBookTable;
