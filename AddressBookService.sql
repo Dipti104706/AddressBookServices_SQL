bd877aa36b97359f6eee3d05eff43be45216528b
Dipti104706 <diptimayeebehura104@gmail.com>
2021-11-14 20:22:05 +0530

    [Dipti] added code for ensuring all retrieve quaries done from Uc-6 to Uc-9 in the address book

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -72,3 +72,90 @@ Select count(Type)as Count,Type  from AddressBookTable group by Type;
 insert into AddressBookTable values('Debasmita','Sutar','Jagda','Rourkela','Odisha',769042,258721564,'deba12@gmail.com','closefriend','Friend');
 select * from AddressBookTable;
 
+-------Using ER Diagram----------Avoid duplicate-----
+--------------Create Address_Book------------------
+create table Address_Book(
+AddressBookID int identity(1,1) primary key,
+AddressBookName varchar(100)
+)
+---Insert the values in Address_Book
+Insert into Address_Book values ('Diptimayee'),('Puja'),('Swornalata');
+---Retrieve the data
+select * from Address_Book;
+
+--Create Contact_Person Table-------
+create table Contact_Person(
+Book_ID int,
+ContactID int identity(1,1) primary key,
+FirstName varchar(100),
+LastName varchar(100),
+Address varchar(250),
+City varchar(100),
+State varchar(100),
+Pin BigInt,
+PhoneNumber BigInt,
+EmailId varchar(200),
+foreign key (Book_ID) references Address_Book(AddressBookID));
+
+select * from Contact_Person;
+
+--Inserting contact details------- 
+Insert into Contact_Person values('1','Sarika','Boxi','Jhirpani','Rourkela','Odisha',769042,458721564,'sarikabx12@gmail.com'),
+('3','Sujata','Patra','Jhirpani','Rourkela','Odisha',769042,3587215654,'sujatax14@gmail.com'),
+('1','Debasmita','Sutar','Jagda','Rourkela','Odisha',769042,258721564,'deba12@gmail.com'),
+('2','Suchitra','Mallick','Kalpana','Bhubaneswar','Odisha',751042,458721564,'suchim12@gmail.com');
+
+--Retrieve the data
+select * from Contact_Person;
+
+---Create Relation_type table------
+create table Relation_type
+(TypeID int identity(1,1) primary key,
+ TypeName varchar(100)
+ );
+---Insert the values in contat_type
+Insert into Relation_type values ('Family'),('Friends'),('Profession');
+--Retrieve the data
+Select * from Relation_type;
+
+---Create relation type table
+create Table Contact_Type(
+ContactType_ID int,
+Contact_ID int,
+foreign key (ContactType_ID) references Relation_type(TypeID),
+foreign key (Contact_ID) references Contact_Person(ContactID)
+)
+---Insert the values in relation_type
+insert into Contact_Type values (1,3),(2,3),(3,1),(1,2),(2,4);
+--Retrieve the data
+Select * from Contact_Type;
+
+----------------UC13---------------
+-------------Redo UC6-10 with new tables----------
+----UC6------------------
+-------Retrieve Person Belonging to  City or State------
+select Address_Book.AddressBookName,FirstName,LastName,Address,City,State,Pin,PhoneNumber,EmailId,TypeName
+from Contact_Person 
+INNER JOIN  Address_Book on Address_Book.AddressBookID=Contact_Person.Book_ID where City='Bhubaneswar' 
+INNER JOIN Contact_Type on Contact_Type.Contact_ID=Contact_Person.ContactID
+INNER JOIN Relation_type on Contact_Type.ContactType_ID=Relation_type.TypeID
+
+----UC7--->Size of state and city
+select Count(*)As CountOfStateAndCity ,State,City
+from Contact_Person 
+INNER JOIN  Address_Book on Address_Book.AddressBookID=AddressBook_ID Group by State,City
+
+-----UC8--->Retrieve the person data entries sorted alphabetically
+select AddressBookName,FirstName,LastName,Address,City,StateName,ZipCode,PhoneNum,EmailId,ContactTypeName
+from Contact_Person 
+INNER JOIN  Address_Book on Address_Book.AddressBookID=AddressBook_ID 
+INNER JOIN Relation_Type on Relation_Type.Contact_Id=Contact_Person.ContactID
+INNER JOIN Contact_Type on Relation_Type.ContactType_ID=Contact_Type.ContactTypeID
+order by(FirstName)
+---	UC9--->Get Number Of contact persons by type
+select Count(*) as NumberOfContacts,Contact_Type.ContactTypeName
+from Contact_Person 
+INNER JOIN  Address_Book on Address_Book.AddressBookID=Contact_Person.AddressBook_ID
+INNER JOIN Relation_Type on Relation_Type.Contact_ID=Contact_Person.ContactID
+INNER JOIN Contact_Type on Relation_Type.ContactType_ID=Contact_Type.ContactTypeID
+Group by ContactTypeName
